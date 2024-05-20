42383ee863a26216d5bd43b373b438340ee24ad7
je4npw <je4n.pw@gmail.com>
2024-05-19 21:28:27 -0300

    #8.2(feat) - prototipando form de usuário e grid de acolhidos

--- app/Livewire/Usuario.php
+++ app/Livewire/Usuario.php
@@ -2,12 +2,34 @@
 
 namespace App\Livewire;
 
+use App\Http\Controllers\UserController as UC;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Usuario extends Component
 {
+    public string $labelStyle="block mb-2 text-sm font-medium text-gray-900 dark:text-white";
+    public string $inputStyle="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500";
+    public string $saveButton="";
+    public string $cancelButton="";
+
+    public function save()
+    {
+
+    }
     public function render()
     {
-        return view('livewire.usuario');
+        $user = new UC();
+
+        $userId = Auth::user()->id;
+
+        $arr = $user->getById($userId);
+
+        return view('livewire.usuario')->with([
+            'id' => $arr->id,
+            'name' => $arr->name,
+            'email' => $arr->email,
+            'avatar' => $arr->avatar
+        ]);
     }
 }
