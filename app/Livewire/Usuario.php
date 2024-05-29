ef0662981002d9f1a2bce303926923ac9d81e847
je4npw <je4n.pw@gmail.com>
2024-05-28 21:44:30 -0300

    #11.4(chorando) - só Deus sabe o quanto eu chorei pra fazer esses selects funcionarem

--- app/Livewire/Usuario.php
+++ app/Livewire/Usuario.php
@@ -2,15 +2,16 @@
 
 namespace App\Livewire;
 
+use App\Models\City;
+use App\Models\State;
 use App\Models\User;
-use Gabrielmoura\LaravelCep\Cep;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class Usuario extends Component
 {
     public $activeTab = 'basic';
-
     // form properties
     public $avatar;
     public $name;
@@ -24,6 +25,10 @@ class Usuario extends Component
     public $cpf;
     public $rg;
     public $email;
+    public $city_name;
+    public $state_name;
+    public $states = [];
+    public $cities = [];
 
 
     public function save()
@@ -36,16 +41,21 @@ class Usuario extends Component
         try {
             $user = User::findOrFail($userId);
 
+            $userAddress = $user->address()->first();
+
+            $this->states = State::all();
+
+            $this->cities = $this->uf ? City::where('state_id', $this->uf)->get() : [];
+
             $this->fill([
                 'avatar' => $user->avatar,
                 'name' => $user->name,
                 'birth_date' => $user->birth_date,
-                'address' => $user->address,
-                'address_number' => $user->address_number,
-                'cep' => $user->cep,
-                'neighborhood' => $user->neighborhood,
-                'city' => $user->city,
-                'uf' => $user->uf,
+                'address' => $userAddress ? $userAddress->address : '',
+                'address_number' => $userAddress ? $userAddress->address_number : '',
+                'neighborhood' => $userAddress ? $userAddress->neighborhood : '',
+                'uf' => $this->uf,
+                'city' => $this->city,
                 'cpf' => $user->cpf,
                 'rg' => $user->rg,
                 'council_registry' => $user->council_registry,
@@ -57,27 +67,50 @@ class Usuario extends Component
                 'permission_group_id' => $user->permission_group_id,
                 'bank' => $user->bank,
                 'bank_agency' => $user->bank_agency,
-                'bank_account' => $user->bank_account,
+                'bank_account' => $user->bank_account
             ]);
         } catch (ModelNotFoundException $e) {
             // Lida com o caso em que o usuário não foi encontrado
             session()->flash('error', 'Usuário não encontrado');
+            toast()->warning('Usuário não encontrado', 'Atenção')->push();
         } catch (\Exception $e) {
             // Lida com outros tipos de exceções
             session()->flash('error', 'Ocorreu um erro ao buscar os dados do usuário');
+            toast()->debug('Erro ao buscar usuário')->push();
+
         }
+    }
+
+    public function updatedUf($value)
+    {
+        $this->cities = City::where('state_id', $value)->get();
+        $this->city = '';
+        $this->city_name = '';
     }
 
     public function searchCep()
     {
-        $cep = Cep::find($this->cep);
+//        try {
+//
+//            if ($this->cep) {
+//
+//                $cep = Cep::find($this->cep);
+//
+//                $this->fill([
+//                    'address' => $cep->logradouro,
+//                    'neighborhood' => $cep->bairro,
+//                    'city' => $cep->localidade,
+//                    'uf' => $cep->uf,
+//                ]);
+//
+//            }
+//
+//        } catch (\JsonException $e) {
+//
+//            toast()->debug($e)->push();
+//
+//        }
 
-        $this->fill([
-            'address' => $cep->logradouro,
-            'neighborhood' => $cep->bairro,
-            'city' => $cep->localidade,
-            'uf' => $cep->uf,
-        ]);
     }
 
     public function switchTab($tab): void
@@ -87,6 +120,10 @@ class Usuario extends Component
 
     public function render()
     {
+        if (!empty($this->uf)){
+            $this->cities = City::where('state_id', $this->uf)->get();
+        }
+
         return view('livewire.usuario');
 
     }
